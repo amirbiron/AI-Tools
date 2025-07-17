39d39102972e27dd67f0a27287327b55ea8183ca
amir haim <amirbiron@gmail.com>
2025-07-18 02:23:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -563,6 +563,6 @@ def health():
     }
     return jsonify(status)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+# if __name__ == '__main__':
+#     # Start the Flask app
+#     app.run(debug=False, port=5000)
